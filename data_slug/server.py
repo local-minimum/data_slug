f71d6e6cd066ca0618643801acfd95443b89d8fb
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-04-11 00:18:55 +0200

    Added user routes to app

--- data_slug/server.py
+++ data_slug/server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from flask import Flask
 from server_modules import core
+from server_modules import users
 
 __LOCAL = True
 __PORT = 5000
@@ -18,6 +19,8 @@ if __name__ == "__main__":
     app = get_app(debug=__DEBUG)
 
     core.monkey_patch_app(app)
+
     core.register_routes(app)
+    users.register_routes(app)
 
     app.run(host="127.0.0.1" if __LOCAL else "0.0.0.0", port=__PORT)
