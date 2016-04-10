c4d48222916d567c4af8b7348cc08304d9963ce3
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-04-10 15:38:24 +0200

    Basic app

--- data_slug/server.py
+++ data_slug/server.py
@@ -3,11 +3,13 @@
 from flask import Flask, request
 
 
-def get_app():
+def get_app(local=False, port=80, debug=False):
 
-    pass
+    app = Flask("127.0.0.1" if local else "0.0.0.0", port=port)
+    app.debug = debug
+    return app
 
 if __name__ == "__main__":
 
-    app = get_app()
+    app = get_app(local=True, debug=True)
     app.run()
