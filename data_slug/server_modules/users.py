d4f9d70778c12a91dd45e59fdd66a9e1282a3bbc
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-04-10 17:51:39 +0200

    Idea for checking if users

--- data_slug/server_modules/users.py
+++ data_slug/server_modules/users.py
@@ -1,15 +1,18 @@
 import mysql.connector as mariadb
+import sql_commands
 
 __USER_TABLE = "Users"
 
 
-def has_any_users(app):
+def has_any_users(app, project=""):
 
     cursor = app.db.cursor
     if cursor:
 
         try:
-            cursor.select()
+            return cursor.select(
+                sql_commands.table_has_any,
+                app.db.get_table(__USER_TABLE, project)) == "TRUE"
         except mariadb.Error:
             return False
         else:
